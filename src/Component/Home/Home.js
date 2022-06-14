cd2e5b7cd16c6c3c05ad3fa3559a185c2e11ae2f
Jahid-Hasan-Jony <jahidhasanjony48@gmail.com>
2022-06-14 22:52:19 +0600

    added new route and new section

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 import Header from '../Header/Header'
 import Projects from '../Projects/Projects';
+import Sidebar from '../Sidebar/Sidebar';
 const Home = () => {
     return (
         <div>
+            <Sidebar />
             <Header />
-            <h1 className='container text-center' style={{ fontSize: '55px' }}>My Projects</h1>
+            <h1 className='container text-center' style={{ fontSize: '50px' }}>My Projects</h1>
             <Projects />
-            <h1 className='py-4 container text-center' style={{ fontSize: '55px' }}>Contact Me</h1>
+            <h1 className='py-4 container text-center' style={{ fontSize: '50px' }}>Contact Me</h1>
             <ContactForm />
         </div>
     );
---
+++ src/Component/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sidebar.css'
+import { BsGithub, BsFacebook, BsYoutube, BsLinkedin, BsSkype } from 'react-icons/bs'
+import { AiOutlineBars } from 'react-icons/ai'
+import { ImCross } from 'react-icons/im'
+
+const Sidebar = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Sidebar;
